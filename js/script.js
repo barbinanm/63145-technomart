1d7900759627c15855a422efc7f5c603325cfe68
barbinanm <mystic11@yandex.ru>
2017-02-18 14:59:55 +0300

    финальная верстска

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ var feedbackPopup = document.querySelector(".feedback");
 var feedbackButton = document.querySelector(".company-feedback-btn");
 var feedbackClose = document.querySelector(".feedback-close");
 var feedbackCancel = document.querySelector(".feedback-cancel");
-var feedbackLogin = feedbackPopup.querySelector("[name=login]");
- 
+
+if (feedbackButton != null && feedbackClose != null && feedbackCancel != null) {
     feedbackButton.addEventListener("click", function (event) {
         event.preventDefault(event);
         feedbackPopup.classList.add("show-block");
-        feedbackLogin.focus();
     });
 
-   
     feedbackClose.addEventListener("click", function (event) {
         event.preventDefault(event);
         feedbackPopup.classList.remove("show-block");
@@ -28,6 +26,37 @@ var feedbackLogin = feedbackPopup.querySelector("[name=login]");
             }
         }
     });
+}
+
+var buyItemButtons = document.querySelectorAll(".catalog-item-buy"), i;
+var cartBlock = document.querySelector(".cart-notification");
+var cartClose = document.querySelector(".cart-notification-close");
+var cartCancel = document.querySelector(".cart-cancel-btn");
+
+for (i = 0; i < buyItemButtons.length; ++i) {
+	buyItemButtons[i].addEventListener("click", function (event) {
+		event.preventDefault(event);
+		cartBlock.classList.add("show-block");
+	})
+}
+
+cartClose.addEventListener("click", function (event) {
+	event.preventDefault(event);
+	cartBlock.classList.remove("show-block");
+});
+
+cartCancel.addEventListener("click", function (event) {
+	event.preventDefault(event);
+	cartBlock.classList.remove("show-block");
+});
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode == 27) {
+        if (cartBlock.classList.contains("show-block")) {
+            cartBlock.classList.remove("show-block");
+        }
+    }
+});
 
 
 var serviceLinks = document.querySelectorAll(".service-list-column a");
